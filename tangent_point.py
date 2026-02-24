76397bacfd2942e3d989c4df41ed749a93084966
asaha-astro09 <saha.amrita2019@gmail.com>
2026-02-24 17:48:45 +0530

    Changed issue in tangent point calculation

--- tangent_point.py
+++ tangent_point.py
@@ -2,12 +2,12 @@ import numpy as np
 
 R0 = 8.5    #distance between sun and galactic center (in kPc)
 l = float(input('Enter Galactic longitude (degrees): '))    #(in degree)  
-l_rad = np.deg2rad(l)   #converting degree to radian
+l_rad = np.radians(l)   #converting degree to radian
 
 #R=R0*np.sin(l) (tangent point distance)
 
 def tpd (R0,l_rad):
-    R=R0*np.sin(l)
+    R=R0*np.sin(l_rad)
     return R
     
 R = tpd(R0, l_rad)
